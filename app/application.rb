071a50c181d03f68fdd1f443a1c9c8fe9f5833c1
mkopsho <michael.kopsho@gmail.com>
2020-06-10 10:15:50 -0400

    Done.

---
+++ app/application.rb
@@ -0,0 +1,12 @@
+class Application
+  def call(env)
+    resp = Rack::Response.new
+    time = Time.now.hour
+    if time > 12
+      resp.write "Good afternoon!"
+    else
+      resp.write "Good morning!"
+    end
+    resp.finish
+  end
+end
